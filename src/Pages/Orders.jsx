d466cdf268d7c94559fcff1bbdac8a1bf203d2da
PremBhooma <bhoomasagar1213@gmail.com>
2023-12-02 00:19:35 +0530

    orders

--- src/Pages/Orders.jsx
+++ src/Pages/Orders.jsx
@@ -7,6 +7,23 @@ const Orders = () => {
   return (
     <>
       <Navbar />
+
+      <div className="order_id">
+        <div>
+          <div className="order_id_one">
+            <p>Orders</p>
+            <p>&#62;</p>
+            <p>Order 32457ABC</p>
+          </div>
+          <div className="order_id_two">
+            <p>Order 32457ABC</p>
+            <div>
+              <button>Back</button>
+              <button>Approve order</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
