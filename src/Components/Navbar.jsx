09760e338b15f5aaa5dea529a33288c590da1f60
PremBhooma <bhoomasagar1213@gmail.com>
2023-12-02 16:22:17 +0530

    navbarIcon

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -21,56 +21,15 @@ const Navbar = () => {
             className="collapse navbar-collapse navbox"
             id="navbarSupportedContent"
           >
-            {/* <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Link
-                </a>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu">
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
-              </li>
-            </ul> */}
             <div>
               <a>Store</a>
               <a>Orders</a>
               <a>Analytics</a>
             </div>
             <div>
-              <a>Cart</a>
+              <a>
+                <i class="fa-solid fa-cart-shopping"></i>
+              </a>
               <div className="nav-item dropdown">
                 <a
                   className="nav-link dropdown-toggle"
